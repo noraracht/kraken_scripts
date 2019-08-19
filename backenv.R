459e7fd72551020f0d33654f38f02275be0e314f
Siavash Mirarab <smirarab@gmail.com>
2019-08-19 16:23:18 -0700

    update

--- backenv.R
+++ backenv.R
@@ -54,13 +54,45 @@ a = data.frame(a, #rbind(
 a$D=round(1-(1-a$d)^(1/31),3)
 qplot((1-tp),fp,fill=-log(est/cor),label=round(est/cor,digits=2),geom="tile",data=a[xor(a$tp==0 , a$M!="N"),])+ geom_text(color="white",size=2.0)+ facet_grid(D~cl,scales="free_x",shrink=T,space="free_x",labeller=label_both)+theme_classic()+ scale_x_continuous(labels=percent,name=expression("FN rate ("~f[n]~")"))+scale_y_continuous(labels=percent,name=expression("FP portion of genome ("~f[p]~")"))+scale_fill_continuous(name="estimated/correct J",guide="none")+geom_text(aes(x=0.3,y=-0.045,label=round(j(x=d,cl=cl)/j(x=d),2),color=j(x=d,cl=cl)/j(x=d)),size=3.1)+scale_color_continuous(name="estimated/correct J",guide="none")
 
-qplot((1-tp),fp,fill=-(abs((cor-est)/cor))^1/10,label=percent((cor-est)/cor),geom="tile",data=a[xor(a$tp==0 , a$M!="N"),])+ geom_text(color="white",size=2.0)+ facet_grid(D~cl,scales="free_x",shrink=T,space="free_x",labeller=label_both)+theme_classic()+ scale_x_continuous(labels=percent,name=expression("FN rate ("~f[n]~")"))+scale_y_continuous(labels=percent,name=expression("FP portion of genome ("~f[p]~")"))+scale_fill_continuous(name="estimated/correct J",guide="none")+geom_text(aes(x=0.3,y=-0.045,label=percent((j(x=d)-j(x=d,cl=cl))/j(x=d)),color=-(j(x=d)-j(x=d,cl=cl))/j(x=d)^1/10),size=3.1)+scale_color_continuous(name="estimated/correct J",guide="none")
-
+qplot((1-tp),fp,fill=-(abs((cor-est)/cor))^1/10,label=percent((cor-est)/cor),geom="tile",data=a[xor(a$tp==0 , a$M!="N"),])+ geom_text(color="white",size=2.0)+
+  facet_grid(D~cl,scales="free_x",shrink=T,space="free_x",labeller=label_both)+theme_classic()+
+  scale_x_continuous(labels=percent,name=expression("FN rate ("~f[n]~")"))+scale_y_continuous(labels=percent,name=expression("FP portion of genome ("~f[p]~")"))+
+  scale_fill_continuous(name="estimated/correct J",guide="none")+
+  geom_text(aes(x=0.3,y=-0.045,label=percent((j(x=d)-j(x=d,cl=cl))/j(x=d)),color=-(j(x=d)-j(x=d,cl=cl))/j(x=d)^1/10),size=3.1)+scale_color_continuous(name="estimated/correct J",guide="none")
 ggsave("backenvlope-RR.pdf",width=10,height=7)
 
-qplot((1-tp),fp,fill=-log10(((dj(est)-dj(cor))/dj(cor))+0.2),label=percent((dj(est)-dj(cor))/dj(cor)),geom="tile",data=a[xor(a$tp==0 , a$M!="N"),])+ geom_text(color="white",size=2.0)+ facet_grid(D~cl,scales="free_x",shrink=T,space="free_x",labeller=label_both)+theme_classic()+ scale_x_continuous(labels=percent,name=expression("FN rate ("~f[n]~")"))+scale_y_continuous(labels=percent,name=expression("FP portion of genome ("~f[p]~")"))+scale_fill_continuous(name="estimated/correct J",guide="none")+geom_text(aes(x=0.3,y=-0.045,label=percent((dj(j(x=d,cl=cl))-dj(j(x=d)))/dj(j(x=d))),color=-log10((dj(j(x=d,cl=cl))-dj(j(x=d)))/dj(j(x=d))+0.2)),size=3.1)+scale_color_continuous(name="estimated/correct J",guide="none")
+qplot((1-tp),y=abs(((est)-(cor))/(cor)),color=as.factor(-fp),geom="line",data=a[xor(a$tp==0 , a$M!="N"),])+ 
+  facet_grid(D~cl,scales="free_y",labeller=label_both)+
+  theme_classic() +theme(panel.border  = element_rect(fill=NA,size = 1))+
+  scale_x_continuous(labels=percent,name=expression("FN rate ("~f[n]~")"),breaks = c(0.01,0.2,0.4))+
+  scale_y_continuous(labels=percent,name="Relative Jaccard error")+
+  scale_fill_continuous(name="estimated/correct J",guide="none")+
+  geom_hline(aes(yintercept=(j(x=d)-j(x=d,cl=cl))/j(x=d)),color="red",linetype=2)+
+  scale_color_brewer(name=expression("FP ("~f[p]~")"),palette = 1,label=function(x) percent(-as.numeric(x)))
+ggsave("backenvlope-RR-lines.pdf",width=6.5,height=5)
 
+qplot((1-tp),fp,fill=-log10(((dj(est)-dj(cor))/dj(cor))+0.2),label=percent((dj(est)-dj(cor))/dj(cor)),geom="tile",data=a[xor(a$tp==0 , a$M!="N"),])+ geom_text(color="white",size=2.0)+ facet_grid(D~cl,scales="free_x",shrink=T,space="free_x",labeller=label_both)+theme_classic()+ scale_x_continuous(labels=percent,name=expression("FN rate ("~f[n]~")"))+scale_y_continuous(labels=percent,name=expression("FP portion of genome ("~f[p]~")"))+scale_fill_continuous(name="estimated/correct J",guide="none")+geom_text(aes(x=0.3,y=-0.045,label=percent((dj(j(x=d,cl=cl))-dj(j(x=d)))/dj(j(x=d))),color=-log10((dj(j(x=d,cl=cl))-dj(j(x=d)))/dj(j(x=d))+0.2)),size=3.1)+scale_color_continuous(name="estimated/correct J",guide="none")
 ggsave("backenvlope-RR-d.pdf",width=10,height=7)
+
+qplot((1-tp),y=abs((dj(est)-dj(cor))/dj(cor)),color=as.factor(-fp),geom="line",data=a[xor(a$tp==0 , a$M!="N"),])+ 
+  facet_grid(D~cl,scales="free_y",labeller=label_both)+
+  theme_classic() +theme(panel.border  = element_rect(fill=NA,size = 1))+
+  scale_x_continuous(labels=percent,name=expression("FN rate ("~f[n]~")"),breaks = c(0.01,0.2,0.4))+
+  scale_y_continuous(labels=percent,name="Relative distance error")+
+  scale_fill_continuous(name="estimated/correct J",guide="none")+
+  geom_hline(aes(yintercept=((dj(j(x=d,cl=cl))-dj(j(x=d)))/dj(j(x=d)))),color=2,linetype=2)+
+  scale_color_brewer(name=expression("FP ("~f[p]~")"),  palette = 1,label=function(x) percent(-as.numeric(x)))
+ggsave("backenvlope-RR-d-lines.pdf",width=6.5,height=5)
+
+qplot((1-tp),y=abs((dj(est)-dj(cor))/dj(cor)),color=as.factor(-fp),geom="line",data=a[xor(a$tp==0 , a$M!="N"),])+ 
+  facet_grid(D~cl,scales="free_y",labeller=label_both)+
+  theme_classic() +theme(panel.border  = element_rect(fill=NA,size = 1))+
+  scale_x_continuous(labels=percent,name=expression("FN rate ("~f[n]~")"),breaks = c(0.01,0.2,0.4))+
+  scale_y_log10(labels=percent,name="Relative distance error")+
+  scale_fill_continuous(name="estimated/correct J",guide="none")+
+  geom_hline(aes(yintercept=((dj(j(x=d,cl=cl))-dj(j(x=d)))/dj(j(x=d)))),color=2,linetype=2)+
+  scale_color_brewer(name=expression("FP ("~f[p]~")"),  palette = 1,label=function(x) percent(-as.numeric(x)))
+ggsave("backenvlope-RR-d-lines-log.pdf",width=6.5,height=5)
 
 
 cp=c((1:80)/100);
