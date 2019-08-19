bbd806a64f4762bf36ff72e59f648d51ead91fb2
Siavash Mirarab <smirarab@gmail.com>
2019-08-19 16:48:09 -0700

    update

--- E2.R
+++ E2.R
@@ -5,10 +5,10 @@ dm=cbind(melt(d[,c(1:3,4,6)],id.vars = 1:3),melt(d[,c(1:3,5,7)],id.vars = 1:3)[,
 names(dm)[6] <- c("error")
 head(dm)
 qplot(CL/100,(error),data=dm,linetype=variable,shape=bin,color=variable)+geom_line()+
-  facet_grid(Dist~.,scales="free_y")+theme_classic()+
+  facet_grid(Dist~.,scales="free_y")+
   scale_y_continuous(labels=percent,name="Relative error in Skmer distance")+scale_x_continuous(labels=percent,name=expression("Contamination level"~c[l]))+
   scale_color_brewer(name="Filtering", palette = "Set2")+  
   scale_linetype_manual(name="Filtering",values=c(2,1))+scale_shape(name="M")+
-  theme(legend.position = c(.45,.93),legend.direction = "horizontal")
+  theme_classic() +theme(panel.border  = element_rect(fill=NA,size = 1), legend.position = c(.449,.91),legend.direction = "horizontal")
 ggsave("E2.pdf",width = 4.5,height = 6.5)
 
