2105820ac879a8f1b3b2277a9944f1357d13dd90
Siavash Mirarab <smirarab@gmail.com>
2019-08-20 09:15:06 -0700

    update

--- E2.R
+++ E2.R
@@ -8,16 +8,16 @@ qplot(CL/100,(error),data=dm,linetype=variable,shape=bin,color=variable)+geom_li
   facet_grid(Dist~.,scales="free_y")+
   scale_y_continuous(labels=percent,name="Relative error in Skmer distance")+scale_x_continuous(labels=percent,name=expression("Contamination level"~c[l]))+
   scale_color_brewer(name="Filtering", palette = "Set2")+  
-  scale_linetype_manual(name="Filtering",values=c(2,1))+scale_shape(name="M")+
-  theme_classic() +theme(panel.border  = element_rect(fill=NA,size = 1), legend.position = c(.449,.91),legend.direction = "horizontal",panel.grid.major.y = element_line(linetype = 1,size=0.3,color="gray"))
+  scale_linetype_manual(name="Filtering",values=c(2,1))+scale_shape_manual(name="M",values=c(5,3,4,15))+
+  theme_classic() +theme(panel.border  = element_rect(fill=NA,size = 1), legend.position = c(.449,.91),legend.direction = "horizontal",panel.grid.major.y = element_line(linetype = 1,size=0.24,color="gray"))
 ggsave("E2.pdf",width = 4.5,height = 6.5)
 
 qplot(CL/100,sign(error)*sqrt(abs(error)),data=dm,linetype=variable,shape=bin,color=variable)+geom_line()+
   facet_grid(Dist~.,scales="free_y")+
-  scale_y_continuous(labels=percent,name="Relative error in Skmer distance",breaks=sqrt(c(0,0.01,0.1,0.5,1,5,10)))+
+  scale_y_continuous(name="Relative error in Skmer distance",breaks=c(-sqrt(0.05),sqrt(c(0,0.05,0.2,0.5,1,2,5))),labels=function(x) percent(sign(x)*x^2))+
   scale_x_continuous(labels=percent,name=expression("Contamination level"~c[l]))+
   scale_color_brewer(name="Filtering", palette = "Set2")+  
-  scale_linetype_manual(name="Filtering",values=c(2,1))+scale_shape(name="M")+
-  theme_classic() +theme(panel.border  = element_rect(fill=NA,size = 1), legend.position = "bottom",legend.direction = "horizontal",panel.grid.major.y = element_line(linetype = 1,size=0.3,color="gray"))
+  scale_linetype_manual(name="Filtering",values=c(2,1))+scale_shape_manual(name="M",values=c(5,3,4,15))+
+  theme_classic() +theme(panel.border  = element_rect(fill=NA,size = 1), legend.position = "bottom",panel.grid.major.y = element_line(linetype = 1,size=0.24,color="gray"))
 ggsave("E2-sqrt.pdf",width = 4.5,height = 6.5)
 
