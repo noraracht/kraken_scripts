12c9b7819a348edd895b10d8ac3ecb7b6854f91e
Siavash Mirarab <smirarab@gmail.com>
2019-08-20 20:17:43 -0700

    update

--- Cmnds_inScript_E1.R
+++ Cmnds_inScript_E1.R
@@ -2,6 +2,22 @@ require(ggplot2); require(scales); require(reshape2)
 
 d=read.csv("summary_report_kraken_viral_dist_unmasked_0.0_corrected_k35.csv")
 names(d)
-#melt((d[,c(2,8,30,3,38)]),id.vars = c("Exclude","C_sequences_prct","prct_species"))
-qplot(value,C_sequences_prct/100,color=prct_species,data=m[m$variable!="mash_10K_distance" | m$value<0.5,])+facet_wrap(~variable,scales = "free_x",labeller = function(x) {x$"variable"=c("Phylogenetic","Sequence (Mash computed)");x})+theme_bw()+geom_smooth(se =F,color="red")+ylab("Percent classified")+scale_y_continuous(labels=percent)+xlab("Distance to the closest reference species")+scale_color_continuous(name="Classified at species level")+theme(legend.position = "bottom")
-ggsave("graph3.pdf",width = 5.5, height = 3.5)
+
+qplot(value,C_sequences_prct/100,color=prct_species,data=m[m$variable=="mash_10K_distance" & m$value<.3,])+
+  #facet_wrap(~variable,scales = "free_x",nrow=2,labeller = function(x) {x$"variable"=c("Marker-based phylogenetic distance","Genome-wide Skmer distance");x})+
+  theme_bw()+theme(legend.position = "bottom")+
+  geom_smooth(se =F,color="red", method="lm")+
+  ylab(expression("Percent classified by Krarken"~(1-f[n])))+xlab("Distance to the closest reference species")+
+  scale_y_continuous(labels=percent)+
+  scale_color_continuous(name="Species Classification")
+ggsave("classified-vs-match.pdf",width = 3, height = 3.5)
+
+qplot(value,C_sequences_prct/100,color=prct_species,data=m)+
+  facet_wrap(~variable,scales = "free_x",nrow=1,labeller = function(x) {x$"variable"=c("Marker-based phylogenetic distance","Genome-wide Skmer distance");x})+
+  theme_bw()+theme(legend.position = "bottom")+
+  geom_smooth(se =F,color="red", method="lm",data=m[m$value<0.3,])+
+  ylab("Percent classified")+xlab("Distance to the closest reference species")+
+  scale_y_continuous(labels=percent)+
+  scale_color_continuous(name="Species Classification")
+ggsave("classified-vs-match-full.pdf",width = 6, height = 4.5)
+
