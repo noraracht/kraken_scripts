74e0ffcd2f3b73dc5f783f8424b5ec03725df12e
Siavash Mirarab <smirarab@gmail.com>
2019-08-21 10:49:20 -0700

    updates

--- Cmnds_inScript_E1.R
+++ Cmnds_inScript_E1.R
@@ -7,10 +7,10 @@ qplot(value,C_sequences_prct/100,color=prct_species,data=m[m$variable=="mash_10K
   #facet_wrap(~variable,scales = "free_x",nrow=2,labeller = function(x) {x$"variable"=c("Marker-based phylogenetic distance","Genome-wide Skmer distance");x})+
   theme_bw()+theme(legend.position = "bottom")+
   geom_smooth(se =F,color="red", method="lm")+
-  ylab(expression("Percent classified by Krarken"~(1-f[n])))+xlab("Distance to the closest reference species")+
+  ylab(expression("Reads classified by Krarken"~(1-f[n])))+xlab("Distance to the closest reference species")+
   scale_y_continuous(labels=percent)+
   scale_color_continuous(name="Species Classification")
-ggsave("classified-vs-match.pdf",width = 3, height = 3.5)
+ggsave("classified-vs-match.pdf",width = 4*1.25, height = 3.5*1.25)
 
 qplot(value,C_sequences_prct/100,color=prct_species,data=m)+
   facet_wrap(~variable,scales = "free_x",nrow=1,labeller = function(x) {x$"variable"=c("Marker-based phylogenetic distance","Genome-wide Skmer distance");x})+
