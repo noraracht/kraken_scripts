cceb249071915f554b36e316c3cb66fa90e9d83e
Siavash Mirarab <smirarab@gmail.com>
2019-09-11 18:42:12 -0700

    update

--- Cmnds_inScript_E2_extended.R
+++ Cmnds_inScript_E2_extended.R
@@ -17,12 +17,19 @@ qplot(FPR,recall,shape=as.factor(-k),color=factor(bin),group=bin,data=r[r$FPR<0.
   theme_classic() +theme(panel.border  = element_rect(fill=NA,size = 1), legend.position = "bottom")
 ggsave("ROC-peralpha_extended.pdf",width = 9,height = 3.5)
 
-qplot(FPR,recall,shape=as.factor(confidence_level),color=factor(k),group=k,data=r[r$FPR<0.2&r$confidence_level<0.4& !r$k %in% c(20,27,29),])+
+qplot(FPR,recall,shape=as.factor(confidence_level),color=factor(k),group=k,data=r[r$FPR<0.3&r$confidence_level<0.4& !r$k %in% c(20,27,29),])+
   facet_wrap(~bin,nrow=2,labeller = function(x) {x$bin=c("0","(0-1]","(1-2]","(2-3]","(3-5]","(5-10]","(10-15]","(15-20]","(20-25]",">25");x})+
   scale_shape_manual(name="confidence",values=c(1,0,2,6,5,4))+scale_color_brewer(palette = "Dark2", name="k")+
   theme_classic() +theme(panel.border  = element_rect(fill=NA,size = 1), legend.position = "bottom")+
   scale_x_continuous(breaks=c(0.01,0.09,0.17))
 ggsave("ROC-perbin_extended.pdf",width=8,height = 4.8)
+
+qplot(FPR,recall,shape=as.factor(confidence_level),color=factor(k),group=k,data=r[r$FPR<0.3&r$confidence_level<0.4& !r$k %in% c(20,27,29),])+
+  facet_wrap(~bin,ncol=4,labeller = function(x) {x$bin=c("0","(0-1]","(1-2]","(2-3]","(3-5]","(5-10]","(10-15]","(15-20]","(20-25]",">25");x})+
+  scale_shape_manual(name=expression(alpha),values=c(1,0,2,6,5,4))+scale_color_brewer(palette = "Dark2", name="k")+
+  theme_classic() +theme(panel.border  = element_rect(fill=NA,size = 1), legend.position = c(.76,.13),legend.direction = "horizontal",legend.box.background =  element_rect(linetype = 2))+
+  scale_x_continuous()
+ggsave("ROC-perbin_extended-col.pdf",width=6,height =5)
 
 qplot(FPR,recall,shape=as.factor(confidence_level),color=factor(bin),group=bin,data=r[r$FPR<0.2&r$confidence_level<0.4& !r$k %in% c(20,27,29),])+
   facet_wrap(~k,nrow=1,labeller = label_both)+geom_line()+
@@ -31,6 +38,15 @@ qplot(FPR,recall,shape=as.factor(confidence_level),color=factor(bin),group=bin,d
   theme_classic() +theme(panel.border  = element_rect(fill=NA,size = 1), legend.position = "bottom")
 ggsave("ROC-perk_extended.pdf",width = 9,height = 3.5)
 
+ggplot(aes(x=as.factor(bin),group=interaction(confidence_level,k),color=interaction(-confidence_level,k,sep=" / ")),data=r[r$confidence_level %in% c(0,0.05)& r$k %in% c(28,32,35),])+
+  geom_line(aes(y=recall,linetype="Recall"))+ geom_point(aes(y=recall,group=1,shape="Recall"))+
+  geom_line(aes(y=FPR,linetype="FPR"))+ geom_point(aes(y=FPR,group=1,shape="FPR"))+
+  scale_shape_discrete(name="")+scale_linetype_manual(name="",values=c(3,1))+
+  scale_x_discrete(name="Distance to the closest match (M)",labels=c("0","(0-1]","(1-2]","(2-3]","(3-5]","(5-10]","(10-15]","(15-20]","(20-25]",">25"))+
+  theme_classic() +theme(panel.border  = element_rect(fill=NA,size = 1))+
+  scale_y_continuous(name="Recall",labels=percent,sec.axis = sec_axis(~.*1, name = "FPR", labels = percent, breaks = c(0,0.045,0.086,.54,1)))+
+  scale_color_brewer(palette = "Paired", name=expression(alpha~"/"~k),labels=function(x) {gsub(pattern = "(.*) / (.*)","\\2 \\1",sub(pattern = "-","",x))})
+ggsave("Recall-FPR.pdf",width = 6.9, height = 4.5)
 
 
 qplot(FPR,recall,shape=as.factor(-k),color=factor(bin),group=bin,data=r[r$FPR<1.1&r$confidence_level<0.4&r$k!=20&r$k!=23,])+
