73910367839c2ed3d150b58402cdfdb4958b45ee
Siavash Mirarab <smirarab@gmail.com>
2019-09-13 13:19:23 -0700

    push

--- Cmnds_inScript_E2_extended.R
+++ Cmnds_inScript_E2_extended.R
@@ -37,7 +37,7 @@ qplot(FPR,recall,shape=as.factor(confidence_level),color=factor(k),group=k,data=
   scale_shape_manual(name=expression(alpha),values=c(1,0,2,6,5,4))+scale_color_brewer(palette = "Dark2", name="k", direction = -1)+
   theme_classic() +theme(panel.border  = element_rect(fill=NA,size = 1), legend.position = c(.76,.13),legend.direction = "horizontal",legend.box.background =  element_rect(linetype = 2))+
   scale_x_continuous(breaks=c(0.2,0.5,0.8))
-ggsave("ROC-perbin_extended-col.pdf",width=6.5,height =5.2)
+ggsave("ROC-perbin_extended-col.pdf",width=6.5,height =5.3)
 
 qplot(FPR,recall,shape=as.factor(confidence_level),color=factor(bin),group=bin,data=r[r$FPR<0.2&r$confidence_level<0.4& !r$k %in% c(20,27,29),])+
   facet_wrap(~k,nrow=1,labeller = label_both)+geom_line()+
@@ -49,12 +49,12 @@ ggsave("ROC-perk_extended.pdf",width = 9,height = 3.5)
 ggplot(aes(x=as.factor(bin),group=interaction(confidence_level,k),shape=as.factor(confidence_level),color=interaction(k,sep=" / ")),data=r[r$confidence_level %in% c(0,0.05)& r$k %in% c(28,32,35),])+
   geom_line(aes(y=recall,linetype="Recall"))+ geom_point(aes(y=recall,group=1))+
   geom_line(aes(y=FPR,linetype="FPR"))+ geom_point(aes(y=FPR,group=1))+
-  scale_shape_manual(name=expression(alpha),values=values=c(1,0))+scale_linetype_manual(name="",values=c(3,1))+
+  scale_shape_manual(name=expression(alpha),values=c(1,0))+scale_linetype_manual(name="",values=c(3,1))+
   scale_x_discrete(name="Distance to the closest match (M)",labels=c("0","(0-1]","(1-2]","(2-3]","(3-5]","(5-10]","(10-15]","(15-20]","(20-25]",">25"))+
   theme_classic() +theme(panel.border  = element_rect(fill=NA,size = 1),axis.text.x = element_text(angle=45,hjust = 1,vjust=1), legend.box.background =  element_rect(linetype = 2), legend.position = c(.29,.3),legend.direction = "horizontal", legend.margin = margin(.0,10,.0,5), legend.spacing.y = unit(.5,"pt") )+
   scale_y_continuous(name="Recall",labels=percent,sec.axis = sec_axis(~.*1, name = "FPR", labels = percent, breaks = c(0.007,0.048,0.086,.539)))+
   scale_color_brewer(palette = "Dark2", name=expression(k),labels=function(x) {gsub(pattern = "(.*) / (.*)","\\2 / \\1",sub(pattern = "-","",x))}, direction = -1)
-ggsave("Recall-FPR.pdf",width = 5, height = 4.2)
+ggsave("Recall-FPR.pdf",width = 5*0.95, height = 4.2*0.95)
 
 ggplot(aes(x=as.factor(bin),group=interaction(k),color=interaction(k,sep=" / ")),data=r[r$confidence_level <0.4 & !r$k %in% c(23,20),])+
   geom_line(aes(y=recall,linetype="Recall"))+ geom_point(aes(y=recall,group=1,shape="Recall"))+
