1d6f79d940736243d2f9e7ea2f29f37844933967
noraracht <43321208+noraracht@users.noreply.github.com>
2019-09-26 14:36:05 -0700

    Add files via upload

--- Stacked_bar_plot_dros_dif_domains.R
+++ Stacked_bar_plot_dros_dif_domains.R
@@ -8,11 +8,16 @@ ggplot(df_dros_domains, aes(x=species, y = values/100, fill = legend_val))+
   geom_bar(stat = "identity")+
   theme_bw()+
   theme(axis.text.x = element_text(angle=45,hjust=1,vjust=1))+
-  theme(plot.margin = margin(0.4, 0.2, -0.2, 0.7, "cm"))+
-  theme (legend.position = c(.5,0.94),legend.direction = "horizontal")+
+  theme(plot.margin = margin(0.4, 0.2, -0.15, 0.5, "cm"))+
+  theme (legend.position = c(.5,0.92), legend.direction = "horizontal",
+         legend.title = element_text(size = 7.0), 
+         legend.text = element_text(size = 7.0))+
+  guides(shape = guide_legend(override.aes = list(size = 1.0)))+
+  guides(color = guide_legend(override.aes = list(size = 1.0)))+
   scale_x_discrete(name="")+
-  scale_y_continuous(label = percent, name="Classified reads (%)", limits = c(0/100, 17/100))+
+  scale_y_continuous(label = percent, name="Classified reads (%)", limits = c(0/100, 18/100))+
   scale_fill_brewer(name = "Classification group",labels = c("archaea", "bacteria", "human", "other", "virus"), palette = "Set2")
-ggsave("Classified_at_D.pdf",width=9,height = 8)  
+ggsave("Classified_at_D.pdf",width=5,height = 4)  
   
+
 
